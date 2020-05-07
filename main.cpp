5ee4da2f98ffa75482128718d26e31cfea00f735
Kacper Kropacz <kacper.krop@gmail.com>
2020-05-07 11:21:15 +0200

    Plansza zrobiona

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int plansza[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
 
 int planszaa()
 {
- system("cls");
+    system("cls");
     cout << "     |     |     " << endl;
     cout << " ";
     switch(plansza[0][0])
@@ -24,8 +24,143 @@ int planszaa()
     break;
     default:
         cout << "   ";
-    }
 
+    }
+    cout << " | ";
+    switch(plansza[0][1])
+    {
+    case 100:
+        cout << "(8)";
+    break;
+    case 2:
+        cout << " X ";
+    break;
+    case 9:
+        cout << " O ";
+    break;
+    default:
+        cout << "   ";
+
+    }
+    cout << " | ";
+    switch(plansza[0][2])
+    {
+    case 100:
+        cout << "(9)" << endl;
+    break;
+    case 2:
+        cout << " X " << endl;
+    break;
+    case 9:
+        cout << " O " << endl;
+    break;
+    default:
+        cout << "   " << endl;
+
+    }
+    cout << "     |     |     " << endl;
+    cout << "-----------------" << endl;
+    cout << "     |     |     " << endl;
+    cout << " ";
+    switch(plansza[1][0])
+    {
+    case 100:
+        cout << "(4)";
+    break;
+    case 2:
+        cout << " X ";
+    break;
+    case 9:
+        cout << " O ";
+    break;
+    default:
+        cout << "   ";
+
+    }
+    cout << " | ";
+    switch(plansza[1][1])
+    {
+    case 100:
+        cout << "(5)";
+    break;
+    case 2:
+        cout << " X ";
+    break;
+    case 9:
+        cout << " O ";
+    break;
+    default:
+        cout << "   ";
+
+    }
+    cout << " | ";
+    switch(plansza[1][2])
+    {
+    case 100:
+        cout << "(6)" << endl;
+    break;
+    case 2:
+        cout << " X " << endl;
+    break;
+    case 9:
+        cout << " O " << endl;
+    break;
+    default:
+        cout << "   " << endl;
+
+    }
+    cout << "     |     |     " << endl;
+    cout << "-----------------" << endl;
+    cout << "     |     |     " << endl;
+    cout << " ";
+    switch(plansza[2][0])
+    {
+    case 100:
+        cout << "(1)";
+    break;
+    case 2:
+        cout << " X ";
+    break;
+    case 9:
+        cout << " O ";
+    break;
+    default:
+        cout << "   ";
+
+    }
+    cout << " | ";
+    switch(plansza[2][1])
+    {
+    case 100:
+        cout << "(2)";
+    break;
+    case 2:
+        cout << " X ";
+    break;
+    case 9:
+        cout << " O ";
+    break;
+    default:
+        cout << "   ";
+
+    }
+    cout << " | ";
+    switch(plansza[2][2])
+    {
+    case 100:
+        cout << "(3)" << endl;
+    break;
+    case 2:
+        cout << " X " << endl;
+    break;
+    case 9:
+        cout << " O " << endl;
+    break;
+    default:
+        cout << "   " << endl;
+    }
+    cout << "     |     |     " << endl;
+    return 0;
 }
 
 int main()
